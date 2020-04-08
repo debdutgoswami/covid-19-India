c3a8aa651d56e2b840c96f6c942adf7b9554d730
Debdut Goswami <debdutgoswami@gmail.com>
2020-04-09 03:28:45 +0530

    PyPi

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="covid_india",
-    version="1.0.0",
+    version="1.0.1",
     author="Debdut Goswami",
     author_email="debdutgoswami@gmail.com",
     description="A package to provide information regarding COVID-19 cases in India.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/debdutgoswami/covid-india",
-    download_url = 'https://github.com/debdutgoswami/covid-india/archive/v_0.0.1.tar.gz',
+    url="https://github.com/debdutgoswami/covid-19-India",
+    download_url = 'https://github.com/debdutgoswami/covid-19-India/archive/v_1.0.1.tar.gz',
     packages=setuptools.find_packages(),
     install_requires=[
         'beautifulsoup4',
