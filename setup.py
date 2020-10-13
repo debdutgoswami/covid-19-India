7d14fbd4b37defae6088cbb135a844638906ec26
Debdut Goswami <debdutgoswami@gmail.com>
2020-10-13 23:04:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="covid_india",
-    version="1.1.3",
+    version="1.1.4",
     author="Debdut Goswami",
     author_email="debdutgoswami@gmail.com",
     description="A package to provide information regarding COVID-19 cases in India.",
